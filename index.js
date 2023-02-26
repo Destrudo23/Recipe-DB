b49b33c5455f03e807c60bd5f6fbc3df81ad1bea
Destrudo23 <51265520+Destrudo23@users.noreply.github.com>
2023-02-26 17:35:11 +0100

    3 files modified

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import './style.css';
 
+const searchBtn = document.getElementById("search-btn")
+const inputEl = document.getElementById("search-input")
+
+
+searchBtn.addEventListener("click", function(){
+  console.log("klick")
+})
